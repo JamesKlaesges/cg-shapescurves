8177910c7622d38217d2741787d74c022a8455f4
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-02-13 20:17:03 -0800

    Update renderer.js

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -97,7 +97,7 @@ class Renderer {
         var angle = 360/num_points;
         var currentAngle = angle;
         var i;
-        for (i=0; i<num_points+1; i++){
+        for (i=0; i<num_points; i++){
                current = {x: center.x + (radius * Math.cos(currentAngle)), y: center.y + (radius * Math.sin(currentAngle))};
                this.drawLine(previous, current, color, framebuffer);
                console.error('Plotting from x = ' + previous.x + ', y = ' + previous.y + ' to point x = ' + current.x + ', y = ' + current.y);
