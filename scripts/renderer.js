2ff2ea018fe83bd52be9f7b21f005e4830c95a92
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-02-14 16:55:34 -0800

    Update renderer.js

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -129,7 +129,7 @@ class Renderer {
         pt0 = {x:500, y:275};
         pt1 = {x:475, y:275};
         pt2 = {x:460, y:285};
-        pt3 = {x:450, y:290};
+        pt3 = {x:445, y:290};
         this.drawBezierCurve(pt0, pt1, pt2, pt3, color, framebuffer);
         
         //s
